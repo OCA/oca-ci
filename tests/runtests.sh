f890cdba23d1158499649a20a071413db68d031e
Stéphane Bidoul <stephane.bidoul@gmail.com>
2024-03-17 14:53:50 +0100

    Refactor runtests.sh

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -6,8 +6,9 @@ set -e
 # which is assumed to be mounted somewhere in the docker image.
 
 here=$(dirname $0)
-testvenv=/tmp/testvenv 
 
+testvenv=/tmp/testvenv 
 /usr/bin/python3 -m venv $testvenv
-$testvenv/bin/pip install pytest
+$testvenv/bin/pip install -r $here/requirements.txt
+
 $testvenv/bin/pytest --color=yes --ignore $here/data $here "$@"
